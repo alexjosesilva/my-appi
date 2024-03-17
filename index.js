f9ee978268f1ef437691fc46fcf7d04c7f2888b9
Alex Jose <turmawb@gmail.com>
2024-03-17 08:52:12 -0300

    criacao do front-end com formatacao e utilização da lib dataDatables para desenvolvimento da paginação

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // app.js
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const airplanesRoutes = require('./routes/airplanesRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Adiciona o middleware CORS para permitir todas as origens
+app.use(cors());
 
 // Configuração do view engine EJS
 app.set('view engine', 'ejs');
@@ -14,6 +18,7 @@ app.use(bodyParser.json());
 
 // Configuração do middleware para servir arquivos estáticos
 app.use(express.static('public'));
+app.use(express.static('views'));
 
 // Rotas relacionadas aos aviões
 app.use('/airplanes', airplanesRoutes);
